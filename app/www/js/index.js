760fdfb2ac05b30ba904a746c15593a4468f7246
MKotlik <mkotlik@stuy.edu>
2017-03-23 01:21:11 -0400

    fixed vibration

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,9 +1,10 @@
 var getInfoButton = document.getElementById("get-info");
 var infoBox = document.getElementById("info-box");
 
-var onClick = function(e) {
-  console.log("clicked");
-  navigator.vibrate(500); //Vibrate for 0.5 seconds
-};
+function vibrateOnClick(e) {
+    e.preventDefault();
+    console.log("clicked");
+    navigator.vibrate(500); //Vibrate for 0.5 seconds
+}
 
-getInfoButton.addEventListener("click", onClick);
+getInfoButton.addEventListener("click", vibrateOnClick, false);

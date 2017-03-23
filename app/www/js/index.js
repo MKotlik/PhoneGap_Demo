26edc4388bde38ced2ad0732c0ee1c24f1feec42
MKotlik <mkotlik@stuy.edu>
2017-03-23 01:29:32 -0400

    Added device info

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -1,10 +1,20 @@
 var getInfoButton = document.getElementById("get-info");
 var infoBox = document.getElementById("info-box");
 
+/* var serverButton = document.createElement("button");
+serverButton.setAttribute("type", "button");
+serverButton.setAttribute("id", "push-server");
+serverButton.setAttribute("name", "push-server");
+serverButton.innerHTML = "Send to Server"; */
+
 function vibrateOnClick(e) {
-    e.preventDefault();
     console.log("clicked");
     navigator.vibrate(500); //Vibrate for 0.5 seconds
+
+    var model = device.model;
+    var platform = device.platform;
+
+    infoBox.innerHTML = "Say hi to '" + model + "', which is running '" + platform + "'";
 }
 
 getInfoButton.addEventListener("click", vibrateOnClick, false);
